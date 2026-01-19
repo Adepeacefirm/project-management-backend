17d34b52bba325353ddbcf8c741cf52c235efb49
unknown <ade12peace@gmail.com>
2026-01-19 18:46:17 +0100

    dev

--- inngest/index.js
+++ inngest/index.js
@@ -62,7 +62,7 @@ const syncUserUpdation = inngest.createFunction(
       },
       data: {
         email: data?.email_addresses[0]?.email_address,
-        name: data?.first_name + " " + last_name,
+        name: data?.first_name + " " + data?.last_name,
         image: data?.image_url,
       },
     });
