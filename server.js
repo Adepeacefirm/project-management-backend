394d04a5d082c59f3c2171d2f973fa790f1809a7
unknown <ade12peace@gmail.com>
2026-01-19 18:19:08 +0100

    dev

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3800;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
